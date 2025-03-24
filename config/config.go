b9fdf816652f451c68dcf0d87403e2912b2d64eb
Dviih <dviih2s@gmail.com>
2025-03-23 21:17:31 -0300

    Handle KVS unmarshalling.

--- config/config.go
+++ config/config.go
@@ -1,5 +1,8 @@
 package config
 
+import (
+	"gopkg.in/yaml.v3"
+)
 
 type KV struct {
 	Key   string
@@ -7,4 +10,25 @@ type KV struct {
 }
 
 type KVS []*KV
+
+func (kvs *KVS) UnmarshalYAML(value *yaml.Node) error {
+	current := &KV{}
+
+	for _, content := range value.Content {
+		for _, content := range content.Content {
+			if current.Key != "" {
+				current.Value = content.Value
+
+				*kvs = append(*kvs, current)
+				current = &KV{}
+
+				continue
+			}
+
+			current.Key = content.Value
+		}
+	}
+
+	return nil
+}
 
