071da6eb37d11cb24172a62415ac3f09822a6b21
Dviih <dviih2s@gmail.com>
2025-03-27 18:03:10 -0300

    Kernel: Menu() method analogous to `menuconfig`.

--- config/kernel.go
+++ config/kernel.go
@@ -21,3 +21,19 @@ func (kernel *Kernel) Name() string {
 	return kernel.name
 }
 
+func (kernel *Kernel) Menu(ctx context.Context) error {
+	compiler := &Compiler{
+		name:        "menuconfig",
+		project:     kernel.compiler.project,
+		Call:        "make menuconfig",
+		Environment: kernel.compiler.Environment,
+		Arguments:   kernel.compiler.Arguments,
+	}
+
+	if err := compiler.compile(ctx, os.Stdin, os.Stdout, os.Stderr, kernel.Path); err != nil {
+		return err
+	}
+
+	return nil
+}
+
