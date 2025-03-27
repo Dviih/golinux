719abc742bc8e5e5a3a8e96ca4c10bfefc50d114
Dviih <dviih2s@gmail.com>
2025-03-27 17:55:28 -0300

    Kernel structure.

--- config/kernel.go
+++ config/kernel.go
@@ -8,3 +8,12 @@ import (
 	"path"
 )
 
+type Kernel struct {
+	name     string    `yaml:"-"`
+	compiler *Compiler `yaml:"-"`
+
+	Path     string `yaml:"path"`
+	Config   string `yaml:"config"`
+	Compiler string `yaml:"compiler"`
+}
+
