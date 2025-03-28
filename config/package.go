60cf4649c6e473018d458690ba8b7c5bb468f74f
Dviih <dviih2s@gmail.com>
2025-03-28 10:10:38 -0300

    Package structure.

--- config/package.go
+++ config/package.go
@@ -5,3 +5,11 @@ import (
 	"io"
 )
 
+type Package struct {
+	name     string    `yaml:"-"`
+	compiler *Compiler `yaml:"-"`
+	Target   string    `yaml:"target"`
+	Path     string    `yaml:"path"`
+	Compiler string    `yaml:"compiler"`
+}
+
