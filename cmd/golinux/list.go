769f2b9638e4ffb0bda8b1bfb851a7f90260a073
Dviih <dviih2s@gmail.com>
2025-04-01 21:52:48 -0300

    List: Init; Initialize `reflect.Value`.

--- cmd/golinux/list.go
+++ cmd/golinux/list.go
@@ -86,3 +86,131 @@ type List struct {
 	back         *List
 }
 
+
+func (l *List) Init() tea.Cmd {
+	rv := rvAbs(l.rv)
+	delegate := list.NewDefaultDelegate()
+
+	var items []list.Item
+
+	if rv.Type() == reflect.TypeFor[config.KVS]() {
+		for i := 0; i < rv.Len(); i++ {
+			kv := rv.Index(i).Elem()
+
+			items = append(items, &Item{
+				title:       kv.Field(0),
+				description: kv.Field(1),
+			})
+		}
+	} else {
+		switch rv.Kind() {
+		case reflect.Map:
+			m := rv.MapRange()
+
+			for m.Next() {
+				var description interface{}
+
+				mv := rvAbs(m.Value())
+
+				switch mv.Kind() {
+				case reflect.Map:
+					description = "Enter to access Map"
+				case reflect.Slice:
+					description = "Enter to access slice"
+				case reflect.Struct:
+					description = "Enter to access struct"
+				default:
+					key := m.Key()
+					description = func() string {
+						return toString(rv.MapIndex(key))
+					}
+				}
+
+				items = append(items, &Item{
+					title:       m.Key(),
+					description: description,
+				})
+			}
+
+			items = append(items, &createItem{t: rv.Type().Elem()})
+		case reflect.Slice:
+			delegate.ShowDescription = false
+
+			for i := 0; i < rv.Len(); i++ {
+				element := rvAbs(rv.Index(i))
+
+				var title interface{}
+
+				switch element.Kind() {
+				case reflect.Map:
+					title = "Enter to access Map"
+				case reflect.Slice:
+					title = "Enter to access slice"
+				case reflect.Struct:
+					title = "Enter to access struct"
+				default:
+					title = element
+				}
+
+				items = append(items, &Item{
+					title: title,
+				})
+			}
+		case reflect.Struct:
+			rt := rv.Type()
+
+			for i := 0; i < rt.NumField(); i++ {
+				sf := rt.Field(i)
+
+				if !sf.IsExported() {
+					continue
+				}
+
+				field := rv.Field(i)
+
+				var description interface{}
+
+				switch field.Kind() {
+				case reflect.Map:
+					description = "Enter to access map"
+				case reflect.Slice:
+					description = "Enter to access slice"
+				case reflect.Struct:
+					description = "Enter to access struct"
+				default:
+					description = rv.Field(i)
+				}
+
+				items = append(items, &Item{
+					title:       sf.Name,
+					description: description,
+				})
+			}
+		}
+	}
+
+	if l.back != nil {
+		items = append(items, &backItem{})
+	}
+
+	w, h := docStyle.GetFrameSize()
+
+	w = l.size.Width - w
+	h = l.size.Height - h
+
+	if w < 0 {
+		w = 0
+	}
+
+	if h < 0 {
+		h = 0
+	}
+
+	l.list = list.New(items, delegate, w, h-10)
+	l.list.DisableQuitKeybindings()
+	l.list.SetFilteringEnabled(false)
+	l.list.Title = l.title()
+
+	return nil
+}
+
