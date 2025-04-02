f772c52fb377d6b604dae3cea8b8fd8d36047d27
Dviih <dviih2s@gmail.com>
2025-04-01 21:54:13 -0300

    Init exec.

---
+++ cmd/golinux/exec.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"github.com/charmbracelet/bubbles/list"
+	"github.com/charmbracelet/bubbles/viewport"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
+	"io"
+	"strings"
+)
+
